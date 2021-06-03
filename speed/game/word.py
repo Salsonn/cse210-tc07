e16ad5ceccc4fa9d7f5a5fd27c7a8a1dd568d3df
pondels <yeckron@comcast.net>
2021-06-03 17:09:44 -0600

    Things Changed

---
+++ speed/game/word.py
@@ -0,0 +1,21 @@
+import random
+from game import constants
+from game.actor import Actor
+from game.point import Point
+
+class Food(Actor):
+
+    def __init__(self):
+        self._points = 0
+        self.set_text('Word')
+        self.reset()
+
+    def get_points(self):
+        return self._points
+
+    def reset(self):
+        self._points = random.randint(1, 5)
+        x = random.randint(1, constants.MAX_X - 2)
+        y = random.randint(1, constants.MAX_Y - 2)
+        position = Point(x, y)
+        self.set_position(position)
